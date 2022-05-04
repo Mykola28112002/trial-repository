a5183214c3992585c748ed270b12085b7ef3c8e0
Mykola28112002 <95100219+Mykola28112002@users.noreply.github.com>
2022-05-04 16:04:57 +0300

    e

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function getEvenNumbers(start, end) {
+  const result = []
+   // Change code below this line
+  for (const number of getEvenNumbers)
+    console.log(number);
+    if (number % 2 === 0) {
+      result.push(number);
+    }
+  return result;
+    // Change code above this line
+}
+  
+console.log(getEvenNumbers(1, 6))
